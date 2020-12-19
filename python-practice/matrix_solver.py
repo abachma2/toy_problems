97164a14033ce25baea7e0f388ebd0128a3da1f1
abachma2 <abachma2@vols.utk.edu>
2020-12-19 12:17:38 -0600

    no errors raised, but incorrect flux

--- python-practice/matrix_solver.py
+++ python-practice/matrix_solver.py
@@ -4,7 +4,7 @@ import math
 import time
 
 
-class Arrays(N, num_nodes):
+class Arrays(object):
     '''
     Contains contants and methods related to the arrays used with array
     '''
@@ -25,14 +25,14 @@ class Arrays(N, num_nodes):
         self.sigma_t = 0.1
         self.sigma_s = 0.05
         self.q = 1
-        self.x, self.delta_x = np.linspace(-self.a, self.a, num_nodes, \
-                restpe=True)
+        self.x, self.delta_x = np.linspace(-self.a, self.a, num_nodes,
+                                           retstep=True)
 
-        self.phi = np.zeros((num_nodes, 1, mat.ceil(N / 2)))
-        self.A = np.zeros((num_nodes, num_nodes, mat.ceil(N / 2)))
-        self.S = np.zeros((num_nodes, 1, mat.ceil(N / 2)))
+        self.phi = np.zeros((num_nodes, 1, math.ceil(N / 2)))
+        self.A = np.zeros((num_nodes, num_nodes, math.ceil(N / 2)))
+        self.S = np.zeros((num_nodes, 1, math.ceil(N / 2)))
 
-    def matrix_solver(A, S):
+    def matrix_solver(self, A, S):
         '''
         Performs matrix inversion and multiplication to solve the equaition
         Ax = B, arrays must have the same number of rows
@@ -45,10 +45,10 @@ class Arrays(N, num_nodes):
           Array of values not multiplied by the independent variable
         '''
         return np.matmul(np.linalg.inv(A), S)
-    
-    def fill_A_T1(self, A, num_nodes, bc='None'):
+
+    def fill_A_T1(self, num_nodes, bc='None'):
         '''
-        Fills A constant matrix to create a tridiagonal matrix for the T1 
+        Fills A constant matrix to create a tridiagonal matrix for the T1
         approximation
 
         Parameters:
@@ -56,65 +56,73 @@ class Arrays(N, num_nodes):
         A: array
           Constant array to be filled
         num_nodes: int
-          number of spatial nodes in the system, this number is looped over 
+          number of spatial nodes in the system, this number is looped over
           to fill the matrix as needed to account for boundary conditions
-        bc: string 
-          Name of boundary condition to be applied. Default is no boundary 
+        bc: string
+          Name of boundary condition to be applied. Default is no boundary
           conditions are applied.
         '''
         for xx in range(num_nodes):
+            self.A[xx, xx, 0] = -1 / (self.sigma_t * self.delta_x) + \
+                    (self.sigma_t - self.sigma_s) * self.delta_x
             if xx > 0:
-                A[xx, xx-1, 0] = 1/(2*self.Sigma_t*self.delta_x)
-                if xx == num_nodes-1:
-                    A[xx, xx-1, 0] = -1/self.delta_x
+                self.A[xx, xx - 1, 0] = 1 / (2 * self.sigma_t * self.delta_x)
+                if xx == num_nodes - 1:
+                    self.A[xx, xx - 1, 0] = -1 / self.delta_x
                     if bc == 'Marshak':
-                        A[xx, xx, 0] = 1/self.delta_x + 4*self.Sigma_t/np.pi
+                        self.A[xx, xx, 0] = 1 / self.delta_x + \
+                            4 * self.sigma_t / np.pi
                     elif bc == 'Mark':
-                        A[xx, xx, 0] = 1/self.delta_x + np.sqrt(2)*self.Sigma_t
-            elif xx < num_nodes-1:
-                A[xx, xx+1, 0] = 1/(2*self.Sigma_t*self.delta_x)
+                        self.A[xx, xx, 0] = 1 / self.delta_x + \
+                            np.sqrt(2) * self.sigma_t
+            elif xx < num_nodes - 1:
+                self.A[xx, xx + 1, 0] = 1 / (2 * self.sigma_t * self.delta_x)
                 if xx == 0:
-                    A[xx, xx+1, 0] = 1/self.delta_x
-                    if bc =='Marshak':
-                        A[xx, xx, 0] = -1/self.delta_x -4*self.Sigma_t/np.pi
-                    elif bc =='Mark':
-                        A[xx, xx, 0] = -1/self.delta_x - np.sqrt(2)*self.Sigma_t
+                    self.A[xx, xx + 1, 0] = 1 / self.delta_x
+                    if bc == 'Marshak':
+                        self.A[xx, xx, 0] = -1 / self.delta_x - \
+                            4 * self.sigma_t / np.pi
+                    elif bc == 'Mark':
+                        self.A[xx, xx, 0] = -1 / self.delta_x - \
+                            np.sqrt(2) * self.sigma_t
+            
+        return self.A
 
-        return A
-
-    def fill_S_T1(self, S, num_nodes):
+    def fill_S_T1(self, num_nodes):
         '''
-        Fills S constant matrix to create a diagonal matrix for the T1 
+        Fills S constant matrix to create a column matrix for the T1
         approximation
 
         Parameters:
         ===========
-        S: array 
+        S: array
           Constants array to be filled
         num_nodes: int
-          number of spatial nodes in the system 
+          number of spatial nodes in the system
         '''
         for xx in range(num_nodes):
             if xx == 0:
-                S[xx, xx, 0] = 0
-            elif xx == num_nodes-1:
-                S[xx, xx, 0] = 0
-            else: 
-                S[xx, xx, 0] = self.q*self.delta_x
-        return S
-
-
+                self.S[xx, 0, 0] = 0
+            elif xx == num_nodes - 1:
+                self.S[xx, 0, 0] = 0
+            else:
+                self.S[xx, 0, 0] = self.q * self.delta_x
+        return self.S
 
 
 if __name__ == '__main__':
     plt.figure()
-    for bc in ['Marshak','Mark']:
-        tic = time.perf_couter()
-        array = Arrays(1, 100)
-        A = array.fill_A_T1(array.A, 100, bc)
-        S = array.fill_S_T1(array.S, 100)
-        array.phi[:,:, 0] = array.matrix_solver(A, S)
+    for bc in ['Marshak', 'Mark']:
+        tic = time.perf_counter()
+        array = Arrays(1, 10)
+        array.A = array.fill_A_T1(10, bc)
+        array.S = array.fill_S_T1(10)
+        print(array.A[1,:,0])
+        array.phi[:, :, 0] = array.matrix_solver(
+            array.A[:, :, 0], array.S[:, :, 0])
         plt.plot(array.x, array.phi[:, :, 0], label=f'{bc} BC')
+        toc = time.perf_counter()
+        print(f'{toc-tic} s')
     plt.xlabel('Position (cm)')
     plt.ylabel(r'$\phi$(x) $\frac{n}{cm^3}$')
     plt.legend()
